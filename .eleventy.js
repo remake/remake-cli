7f1a3ef011b02fe8eb468814422a6271931468a4
David Miranda <david@storylog.com>
2019-12-04 18:07:00 -0500

    added all pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,15 @@
+const handlebars = require("handlebars");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("site-src/static");
   eleventyConfig.addPassthroughCopy("site-src/CNAME");
   eleventyConfig.addPassthroughCopy("site-src/**/*.{ico,png,xml,webmanifest,svg}");
+
+  handlebars.registerHelper('isPage', function(arg1, arg2, options) {
+    return (arg1.replace(/\//g, "") == arg2.replace(/\//g, "")) ? options.fn(this) : options.inverse(this);
+  });
+
+  eleventyConfig.setLibrary("hbs", handlebars);
 
   return {
     dir: {
