8c00fc397feb442885e77ba7d5b054cf7629549f
David Miranda <david@storylog.com>
2019-12-04 21:44:40 -0500

    mobile nav

---
+++ docs/static/main.js
@@ -0,0 +1,11 @@
+var slideout = new Slideout({
+  'panel': document.getElementById('panel'),
+  'menu': document.getElementById('menu'),
+  'padding': 256,
+  'tolerance': 70
+});
+
+// Toggle button
+document.querySelector('.toggle-button').addEventListener('click', function() {
+  slideout.toggle();
+});
